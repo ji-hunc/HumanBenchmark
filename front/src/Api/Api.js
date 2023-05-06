f58cd36b4050e0fd0702c1a545a9a834cdf2d6d3
jihunchoi <geehoon62017@gmail.com>
2023-05-06 18:35:42 +0900

    feat. saveScore 함수 비로그인시 alert띄우고 return 하게 수정

--- front/src/Api/Api.js
+++ front/src/Api/Api.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 export default {
   saveScore(gameName, userId, score) {
+    if (userId === null) {
+      alert('로그인후 시도해주세요.');
+      return;
+    }
     axios
       .post(
         'https://port-0-humanstats-5x7y2mlh8rjlfi.sel4.cloudtype.app/postScore/',
